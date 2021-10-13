bf8a97c9a8b9022b7d8267ede761bce591f7210e
Ryley <rhorton5@ewu.edu>
2021-10-13 13:32:20 -0700

    Initial File Structure and makefile Made
    I have made started file structure and created a makefile that should work for the homework.
    
    Any commands from pgmProcess.c and pgmUtility.c are just stubbed out for the time being.  main.cu is also created and has a message to make sure it was working.
    
    I also moved any files that were from the zip file; however, didn't seem like it would be directly used for the homework or examples are moved into a resource folder.

---
+++ main.cu
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pgmProcess.h"
+#include "pgmUtility.h"
+
+int main(){
+	printf("If you see me, this means the main file works and every should be compiled.\n");
+	return 0;
+}
